eb30a13def0489fb0fa81134846b2feabc54ab88
Billy <glenn.honore@std.heh.be>
2021-11-02 14:48:10 +0100

    ajout authentification sur route administration

--- routes/Administration.js
+++ routes/Administration.js
@@ -1,10 +1,11 @@
 const express = require('express'),
     router = express.Router(),
+    auth = require("../middleware/auth"),
     controller = require('../controllers/Administration');
 const {ROLE,authRole} = require("../server/AuthNRole");
 
 
 
-router.get('/',authRole(ROLE.ADMIN), controller.index);
+router.get('/',auth, authRole(ROLE.ADMIN), controller.index);
 
 module.exports = router;
